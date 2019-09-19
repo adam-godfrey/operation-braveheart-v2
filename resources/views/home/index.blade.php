125dcaf58430229034de7cda0d0d060835b3693c
Adam Godfrey <adam.godfrey@wiltonbradley.com>
2019-09-19 15:34:08 +0100

    Added About Us button to home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -25,12 +25,14 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
+            <h1>Welcome to {{ Config::get('app.name') }}</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed quisquam ut perspiciatis, repudiandae nulla animi iste vel, praesentium repellendus molestias aliquid consequatur, earum rem qui error voluptates eius enim consequuntur!</p>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex alias, earum consectetur quia natus ducimus voluptate explicabo, hic porro reprehenderit, quasi? Tenetur ipsum distinctio laboriosam perspiciatis officiis dolore, architecto id.</p>
 
             <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam inventore aspernatur repellendus incidunt adipisci modi voluptates recusandae iste eligendi, repudiandae corporis quod aut, optio! Explicabo quaerat unde voluptatem! Itaque, eum!</p>
 
+            <p class="text-center"><a href="{{ route('about.index') }}" class="btn btn-outline-primary">About Us</a></p>
         </div>
     </div>
     <div class="row">
