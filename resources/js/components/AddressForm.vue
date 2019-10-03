292fad6f9040806bc00e95f189dee52cba121d87
Adam Godfrey <adrock952@gmail.com>
2019-10-03 15:07:20 +0100

    Separated out components on memorial form page

---
+++ resources/js/components/AddressForm.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-12">
+                <div class="form-group floating-label-form-group controls">
+                    <label>First Address Line</label>
+                    <input type="text" class="form-control" placeholder="First Address Line" id="address" v-model="fields.address1" v-on:change="address1Change">
+                    <div v-if="errors && errors.address1" class="text-danger">{{ errors.address1[0] }}</div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-group floating-label-form-group controls">
+                    <label>Second Address Line</label>
+                    <input type="text" class="form-control" placeholder="Second Address Line" id="address2" v-model="fields.address2" v-on:change="address2Change">
+                    <div v-if="errors && errors.address2" class="text-danger">{{ errors.address2[0] }}</div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-group floating-label-form-group controls">
+                    <label>Third Address Line</label>
+                    <input type="text" class="form-control" placeholder="Third Address Line" id="address3" v-model="fields.address3" v-on:change="address3Change">
+                    <div v-if="errors && errors.address3" class="text-danger">{{ errors.address3[0] }}</div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-group floating-label-form-group controls">
+                    <label>Town</label>
+                    <input type="text" class="form-control" placeholder="Town" id="town" v-model="fields.town" v-on:change="townChange">
+                    <div v-if="errors && errors.town" class="text-danger">{{ errors.town[0] }}</div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-group floating-label-form-group controls">
+                    <label>County</label>
+                    <input type="text" class="form-control" placeholder="County" id="address" v-model="fields.county" v-on:change="countyChange">
+                    <div v-if="errors && errors.county" class="text-danger">{{ errors.county[0] }}</div>
+                </div>
+            </div>
+        </div>
+        <AddressLookup v-on:populateAddress="getAddress"></AddressLookup>
+    </div>
+</template>
+<script>
+import AddressLookup from './AddressLookup.vue';
+export default {
+    props: ['address1'],
+    data() {
+        return {
+            fields: {
+                address1: '',
+                address2: '',
+                address3: '',
+                town: '',
+                county: '',
+            },
+            errors: {},
+        }
+    },
+    name: 'AddressForm',
+    components: {
+        AddressLookup
+    },
+    methods: {
+        getAddress: function(value) {
+            this.fields.address1 = value.address1
+            this.fields.address2 = value.address2
+            this.fields.address3 = value.address3
+            this.fields.town = value.town
+            this.fields.county = value.county
+        },
+        address1Change: function() {
+            this.$root.$emit('address1Change', this.fields.address1);
+        },
+        address2Change: function() {
+            this.$root.$emit('address2Change', this.fields.address2);
+        },
+        address3Change: function() {
+            this.$root.$emit('address3Change', this.fields.address3);
+        },
+        townChange: function() {
+            this.$root.$emit('townChange', this.fields.town);
+        },
+        countyChange: function() {
+            this.$root.$emit('countyChange', this.fields.county);
+        },
+    },
+    mounted() {
+        this.$root.$on('errors', errors => {
+            this.errors = errors;
+        });
+    },
+}
+</script>
---
+++ resources/js/components/MemorialContactForm.vue
@@ -0,0 +1,89 @@
+<template>
+    <form @submit.prevent="submit">
+        <div class="card mb-4">
+            <div class="row">
+                <div class="col-12">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Contact Name</label>
+                        <input type="text" class="form-control" placeholder="Contact Name" id="contact" v-model="fields.contact">
+                        <div v-if="errors && errors.contact" class="text-danger">{{ errors.contact[0] }}</div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Telephone</label>
+                        <input type="tel" class="form-control" placeholder="Telephone (optional)" id="telephone" v-model="fields.telephone">
+                        <div v-if="errors && errors.telephone" class="text-danger">{{ errors.telephone[0] }}</div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Email Address</label>
+                        <input type="email" class="form-control" placeholder="Email" id="email" v-model="fields.email">
+                        <div v-if="errors && errors.email" class="text-danger">{{ errors.email[0] }}</div>
+                    </div>
+                </div>
+            </div>
+            <AddressForm></AddressForm>
+        </div>
+    </form>
+</template>
+<script>
+import AddressForm from './AddressForm.vue';
+export default {
+    data: function() {
+        return {
+            fields: {
+                rank: '',
+                name: '',
+                dob: '',
+                dod: '',
+                regiment: '',
+                location: '',
+                message: ''
+            },
+            errors: {},
+            maxCharacters: 100,
+        }
+    },
+    name: 'MemorialSubmitForm',
+    components: {
+        AddressForm,
+    },
+    mounted() {
+        this.$root.$on('checkFormsValid', () => {
+            this.errors = {};
+            axios.post('/memorial-garden/send-request', this.fields).then(response => {
+                console.log(response.data);
+                this.$root.$emit('validated')
+            }).catch(error => {
+                if (error.response.status === 422) {
+                    this.errors = error.response.data.errors || {};
+                    this.$root.$emit('errors', this.errors)
+                }
+            });
+        }),
+        this.$root.$on('rankChange', rank => {
+            this.fields.rank = rank;
+        });
+        this.$root.$on('nameChange', name => {
+            this.fields.name = name;
+        });
+        this.$root.$on('dobChange', dob => {
+            this.fields.dob = dob;
+        });
+        this.$root.$on('dodChange', dod => {
+            this.fields.dod = dod;
+        });
+        this.$root.$on('regimentChange', regiment => {
+            this.fields.regiment = regiment;
+        });
+        this.$root.$on('locationChange', location => {
+            this.fields.location = location;
+        });
+        this.$root.$on('messageChange', message => {
+            this.fields.message = message;
+        });
+    }
+}
+</script>
