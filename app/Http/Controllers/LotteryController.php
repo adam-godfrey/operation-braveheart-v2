bd6ade5d9e9ea37d9cbd3ca51689e78c3e9289b2
Adam Godfrey <adrock952@gmail.com>
2019-10-14 06:43:29 +0100

    Linked lottery results to front page

--- app/Http/Controllers/LotteryController.php
+++ app/Http/Controllers/LotteryController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin\LotteryDraw;
+use App\Models\Admin\LotterySetting;
+use App\Models\Admin\LotteryPlayer;
 
 class LotteryController extends Controller
 {
@@ -12,32 +15,57 @@ class LotteryController extends Controller
 
     public function index()
     {
+        $lotteryDraw = LotteryDraw::where('active', 1)->get();
+
+        $draw_date = LotterySetting::select('value')
+                ->where('key', 'draw_date')
+                ->first()
+                ->value;
+
+        foreach(['UK', 'Local'] as $draw_type) {
+            $lotterySettings = LotterySetting::where('key', 'like', $draw_type . '%')
+                ->get();
+
+            $prizes = $lotterySettings->filter(function($item) {
+                return substr($item->key, -5) === 'prize' && $item->value != null;
+            }); 
+
+            $winners = $lotterySettings->filter(function($item) {
+                return substr($item->key, -7) === 'winners';
+            })->first();
+
+            $i = 0;
+            foreach($prizes->take($winners->value) as $prize) {
+                $i++;
+
+                preg_match('~_(.*?)_~', $prize->key, $output);
+
+                $winning_number = LotteryDraw::where('draw_date', $draw_date)
+                    ->where('draw_type', $draw_type)
+                    ->where('active', 1)
+                    ->first()->{$output[1]};
+
+                $player = LotteryPlayer::where('lottery_number', $winning_number)
+                    ->first();
+
+                $result[$draw_type][$output[1]] = (object) [
+                    'winner' => ucwords(strtolower($player->name)),
+                    'prize' => $prize->value,
+                    'number' => $winning_number
+                ];
+            }
+        }
+
         $data =  [
             'page' => (object) [
                 'title' => 'Lottery',
                 'description' => 'Have you won this month\'s lottery?'
             ],
             'video_id' => '10153231379946729',
-            'lottery' => (object) [
-                'prize' => (object) [
-                    'first' => (object) [
-                        'number' => '56',
-                        'prize' => '50',
-                        'winner' => 'Not me'
-                    ],
-                    'second' => (object) [
-                        'number' => '95',
-                        'prize' => '30',
-                        'winner' => 'Not me'
-                    ],
-                    'third' => (object) [
-                        'number' => '62',
-                        'prize' => '20',
-                        'winner' => 'Not me'
-                    ]
-                ]
-            ]
+            'lottery' => (object) $result
         ];
+
+        // dd($data);
 
         return View('lottery.index')->with($data);
     }
